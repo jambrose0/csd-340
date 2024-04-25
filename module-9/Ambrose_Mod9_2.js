3be17edbe5ea4977d76aaabdbd8ed78ecf4252b8
Jacob Ambrose <jambrose0518@gmail.com>
2024-04-24 20:03:21 -0400

    added js file. Styling is in header of html page. Buttons are now clickable and will change the relevant text. External link to Dylan Thomas bio

---
+++ module-9/Ambrose_Mod9_2.js
@@ -0,0 +1,54 @@
+function change1() {
+    var stanza1 = document.getElementById("stanza1");
+    var verse1 = "Old age should burn and rave at close of day;";
+    if(stanza1.innerHTML === "________________________________________") {
+        stanza1.innerHTML = verse1;
+    } else {
+        stanza1.innerHTML = "________________________________________";
+    }
+}
+function change2() {
+    var stanza2 = document.getElementById("stanza2");
+    var verse2 = "Though wise men at their end know dark is right,";
+    if(stanza2.innerHTML === "________________________________________") {
+        stanza2.innerHTML = verse2;
+    } else {
+        stanza2.innerHTML = "________________________________________";
+    }
+}
+function change3() {
+    var stanza3 = document.getElementById("stanza3");
+    var verse3 = "Their frail deeds might have danced in a green bay,";
+    if(stanza3.innerHTML === "________________________________________") {
+        stanza3.innerHTML = verse3;
+    } else {
+        stanza3.innerHTML = "________________________________________";
+    }
+}
+function change4() {
+    var stanza4 = document.getElementById("stanza4");
+    var verse4 = "Wild men who caught and sang the sun in flight,";
+    if(stanza4.innerHTML === "________________________________________") {
+        stanza4.innerHTML = verse4;
+    } else {
+        stanza4.innerHTML = "________________________________________";
+    }
+}
+function change5() {
+    var stanza5 = document.getElementById("stanza5");
+    var verse5 = "Grave men, near death, who see with blinding sight";
+    if(stanza5.innerHTML === "________________________________________") {
+        stanza5.innerHTML = verse5;
+    } else {
+        stanza5.innerHTML = "________________________________________";
+    }
+}
+function change6() {
+    var stanza6 = document.getElementById("stanza6");
+    var verse6 = "Curse, bless, me now with your fierce tears, I pray.";
+    if(stanza6.innerHTML === "________________________________________") {
+        stanza6.innerHTML = verse6;
+    } else {
+        stanza6.innerHTML = "________________________________________";
+    }
+}
